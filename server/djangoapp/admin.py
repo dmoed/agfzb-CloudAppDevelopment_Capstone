5af3b6ca1ffe0319bc98ddbd3cdc4ec35c369741
Don <dmoedjijo@gmail.com>
2022-09-20 23:04:31 -0300

    added models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarModel, CarMake
 
+# from .models import related models
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['manufacturer', 'name', 'dealerId', 'type', 'year', 'trim']
+    #inlines = [CarModelInline]
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['name', 'description', 'country']
+    inlines = [CarModelInline]
 
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 from django.utils.timezone import now
 
-
 # Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
+class CarMake(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    country = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,9 +18,31 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
+class CarModel(models.Model):
+    MODEL_TYPES = (
+        ('sa', 'Saloon'),
+        ('se', 'Sedan'),
+        ('suv', 'SUV'),
+        ('wa', 'Wagon')
+    )
+    MODEL_TRIM = (
+        ('ba', 'Base'),
+        ('sp', 'Sport'),
+        ('lx', 'Luxury'),
+    )
+    id = models.BigAutoField(primary_key=True)
+    manufacturer = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=100)
+    dealerId = models.IntegerField()
+    type = models.CharField(max_length=20, choices=MODEL_TYPES)
+    year = models.DateField()
+    trim = models.CharField(max_length=20, choices=MODEL_TRIM)
+    def __str__(self):
+        return str(self.year.strftime("%Y")) + ' ' + self.name + ' ' + self.get_trim_display() + ' ' + self.get_type_display()
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
